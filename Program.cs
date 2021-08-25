2dcd2fc65f9aff056b421e30086084f98d6a2933
msenat7432 <maxsenat7432@gmail.com>
2021-08-25 09:55:03 -0400

    C#

---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace intro
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            byte number = 11;
+            int count = 11;
+            float totalPrice = 10.99f;
+            char character = 'm';
+            string firstName = "Max";
+            bool isWorking = false;
+
+            Console.WriteLine(number + count + totalPrice);
+            Console.WriteLine(character);
+            Console.WriteLine(firstName);
+            Console.WriteLine(isWorking);
+            Console.WriteLine(firstName[1]);
+            Console.WriteLine("{0} {1}", int.MinValue, int.MaxValue); 
+        }
+    }
+}
